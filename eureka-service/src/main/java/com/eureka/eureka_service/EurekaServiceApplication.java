89f05f171e4e95e63fd48bd7badeae760b5aab7c
shrutiii2901 <136559812+shrutiii2901@users.noreply.github.com>
2026-04-21 11:44:32 +0530

    [Shruti] Added Eureka Service microservice

---
+++ eureka-service/src/main/java/com/eureka/eureka_service/EurekaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.eureka.eureka_service;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
